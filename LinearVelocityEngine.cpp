9889072e6fe2ca9e8f51d83407ce31cb84322674
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2019-05-23 16:23:38 +0200

    Early push

--- LinearVelocityEngine.cpp
+++ LinearVelocityEngine.cpp
@@ -6,9 +6,14 @@
 #include <functional>
 #include <vector>
 #include <chrono>
+#include <thread>
+#include <queue>
+#include <mutex>
 
 #include <immintrin.h>
 #include <immintrin.h>
+
+#define MAX_THREADS 4
 
 using namespace std;
 
@@ -21,21 +26,22 @@ class E19AlexCanut {
 public:
 	vector<Info> p;
 	vector<Info> v;
+	vector<Info> a;
+	int slices;
 
-	E19AlexCanut() {
-
-	}
+	E19AlexCanut() {}
 
 	//vector<Particle> particles;
 	int counter = 0;
 
 	//Add a particle to the system
 	//vy	vx	y	x
-	void Add(float px, float py, float vx, float vy) {
+	void Add(float px, float py, float vx, float vy, float ax, float ay) {
 		//USING SIMD
 		if (counter == 0) {
 			p.push_back({ px, py, 0.0f, 0.0f });
-			v.push_back({ vx, vy, 0.0f,0.0f });
+			v.push_back({ vx, vy, 0.0f, 0.0f });
+			a.push_back({ ax, ay, 0.0f, 0.0f });
 		}
 		else {
 			p[p.size() - 1].particlesF[2] = px;
@@ -43,6 +49,9 @@ public:
 
 			v[p.size() - 1].particlesF[2] = vx;
 			v[p.size() - 1].particlesF[3] = vy;
+
+			a[p.size() - 1].particlesF[2] = ax;
+			a[p.size() - 1].particlesF[3] = ay;
 		}
 		++counter;
 		counter %= 2;
@@ -50,10 +59,23 @@ public:
 
 	//Update all the particles
 	void Update(float dt) {
+
+
+		/*
 		__m128 dtV = _mm_set_ps(dt, dt, dt, dt);
+		{
+			//for (int i = 0; i < p.size(); i++) {
+			//	v[i].particlesSIMD = _mm_fmadd_ps(a[i].particlesSIMD, dtV, v[i].particlesSIMD);
+			//}
+			//for (int i = 0; i < p.size(); i++) {
+			//	p[i].particlesSIMD = _mm_fmadd_ps(v[i].particlesSIMD, dtV, p[i].particlesSIMD);
+			//}
+		}
 		for (int i = 0; i < p.size(); i++) {
+			v[i].particlesSIMD = _mm_fmadd_ps(a[i].particlesSIMD, dtV, v[i].particlesSIMD);
 			p[i].particlesSIMD = _mm_fmadd_ps(v[i].particlesSIMD, dtV, p[i].particlesSIMD);
 		}
+		*/
 	}
 
 	void Run(std::function<void(float x, float y)> f) {
@@ -62,25 +84,70 @@ public:
 			f(p[i].particlesF[2], p[i].particlesF[3]);
 		}
 	}
+
+	void Divide() {
+		slices = p.size() / MAX_THREADS;
+	}
 };
+
+
+std::mutex mut, mut2;
+float dt = 1.0f;
+
+void UpdateThreading(bool* running, queue<vector<Info>>* events, E19AlexCanut* info) {
+	while (*running) {
+		mut2.lock();
+		bool isEmpty = events->empty();
+		mut2.unlock();
+
+		if (events->empty()) {
+			mut.lock();
+			vector<Info> pVector = events->front();
+			events->pop();
+			mut.unlock();
+
+			__m128 dtV = _mm_set_ps(dt, dt, dt, dt);
+			for (int i = 0; i < pVector.size(); i++) {
+				//v[i].particlesSIMD = _mm_fmadd_ps(a[i].particlesSIMD, dtV, v[i].particlesSIMD);
+				//p[i].particlesSIMD = _mm_fmadd_ps(v[i].particlesSIMD, dtV, p[i].particlesSIMD);
+			}
+		}
+	}
+}
+
+
 
 int main() {
 	E19AlexCanut* a = new E19AlexCanut();
+	vector<thread> threadPool;
+	bool running = true;
+	queue<vector<Info>> events;
+
+	for (int i = 0; i < MAX_THREADS; i++){
+		//threadPool.push_back(thread(UpdateThreading, &running, &events, &a));
+	}
 
 	/*BODY*/
-	for (int i = 0; i < 15000000; i++) a->Add(1.0f, 2.0f, 20.0f, 21.0f);
+	for (int i = 0; i < 15000000; i++) a->Add(1.0f, 20.0f, 20.0f, 21.0f, 2.0f, 2.0f);
+	a->Divide();
+	cout << a->slices << endl;
+	cout << a->slices * MAX_THREADS << endl;
 	auto start = chrono::high_resolution_clock::now();
-	for(int i = 0; i<60; i++) a->Update(10.0f);
-	
+	for(int i = 0; i<1; i++) a->Update(1.0f);
+	running = false;
+	for (int i = 0; i < threadPool.size(); i++) {
+		threadPool[i].join();
+	}
 	/*END BODY*/
 
 	auto end = chrono::high_resolution_clock::now();
 	auto finalT = chrono::duration_cast<chrono::milliseconds> (end - start);
 	cout << finalT.count() << " ms" << endl;
-	cout << a->p[0].particlesF[0] <<" "<< a->p[0].particlesF[1] << endl;
+	//cout << a->p[0].particlesF[0] <<" "<< a->p[0].particlesF[1] << endl;
 	//cout << a->p.size() << endl;
 	//cout << a->particles[0].valuesF[0] << ", " << a->particles[0].valuesF[1] << ", " << a->particles[0].valuesF[2] << ", " << a->particles[0].valuesF[3] << endl;
 	//cout << a->particles[0].valuesF[0] << ", " << a->particles[0].valuesF[1] << endl;
 	
+	delete a;
 	return 1;
 }
